f8e5c440e6b4ddd28c82884848431acca48ba45c
robertyang13 <robertyang2023@u.northwestern.edu>
2024-09-28 17:06:47 -0500

    Title Changes

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDS_FsKUPr8ut64m32FrIs1Z5EFb2eZ8qc",
+  authDomain: "robyang13-react-tur.firebaseapp.com",
+  projectId: "robyang13-react-tur",
+  storageBucket: "robyang13-react-tur.appspot.com",
+  messagingSenderId: "237561346587",
+  appId: "1:237561346587:web:192b29016ed911e501d6be"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
